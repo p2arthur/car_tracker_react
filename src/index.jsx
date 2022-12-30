3f0ed0397e9f5abe75168696fcabf09de2f9a415
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-30 20:40:53 -0300

    add basic boilerplate for the car tracker app

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
