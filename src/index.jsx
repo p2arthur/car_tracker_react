88be2083bad3825b574adf973a989a3d0e64b8f3
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-31 14:04:01 -0300

    import store and Provider to be used on the components

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 import App from "./App";
 
@@ -7,4 +9,8 @@ const rootElement = document.getElementById("root");
 
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
