2f4149f386183fcc58aaa6930c45e58696a998d3
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-31 14:04:55 -0300

    implement basic functionality to carForm component - changeName and changeCost

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,5 +1,54 @@
+import { useSelector, useDispatch } from "react-redux";
+import { changeName, changeCost } from "../store";
+
 function CarForm() {
-  return <div>CarForm</div>;
+  const nameInput = useSelector((state) => state.form.name);
+  const costInput = useSelector((state) => state.form.cost);
+  const dispatch = useDispatch();
+
+  const handleNameChange = (event) => {
+    console.log(nameInput);
+
+    dispatch(changeName(event.target.value));
+  };
+  const handleCostChange = (event) => {
+    console.log(costInput);
+    dispatch(changeCost(+event.target.value));
+  };
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Burron</h4>
+      <form className="" action="">
+        <div className="field-group">
+          <div className="field">
+            <label htmlFor="nameInput" className="label">
+              Name
+            </label>
+            <input
+              value={nameInput}
+              onChange={handleNameChange}
+              id="nameInput"
+              className="input is-expanded"
+              type="text"
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="costInput" className="label">
+              Cost
+            </label>
+            <input
+              value={costInput}
+              onChange={handleCostChange}
+              id="costInput"
+              className="input is-expanded"
+              type="number"
+            />
+          </div>
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default CarForm;
