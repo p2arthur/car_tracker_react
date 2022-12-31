c75f43e8d28f5270c815fb79b6b652e04ee84c81
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-30 23:54:18 -0300

    add basic redux toolkit basic boilerplate + add formSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <CarForm />
+      </div>
+      <div>
+        <CarList />
+      </div>
+      <div>
+        <CarSearch />
+      </div>
+      <div>
+        <CarValue />
+      </div>
+    </div>
+  );
 }
 
 export default App;
