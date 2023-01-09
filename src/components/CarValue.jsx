e280191199b504b7a44ca4567e3a9432c8b47bc3
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-01-09 18:51:24 -0300

    add total cost functionality

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+
 function CarValue() {
-  return <div>CarValue</div>;
+  //Filtering the cars that have the cars that includes the search term
+  const totalCost = useSelector(({ cars: { carsList, searchTerm } }) =>
+    carsList
+      .filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      //Using a reduce method to add the cost of them
+      .reduce((accumulator, car) => accumulator + car.cost, 0)
+  );
+
+  return <div className="car-value">Total cost: ${totalCost}</div>;
 }
 
 export default CarValue;
