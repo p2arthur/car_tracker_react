ef681eeb6189f6a74c6f46a3c07db1c070ed2c27
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-01-09 11:42:01 -0300

    add submit functionality to the formcomponent to add a new car to the carsList piece of state

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,31 +1,43 @@
 import { useSelector, useDispatch } from "react-redux";
-import { changeName, changeCost } from "../store";
+import { changeName, changeCost, addCar } from "../store";
 
 function CarForm() {
-  const nameInput = useSelector((state) => state.form.name);
-  const costInput = useSelector((state) => state.form.cost);
+  //Selector functions to get access to the state of carName and carCost
+  const { name, cost } = useSelector((state) => state.form);
+  const { carsList } = useSelector((state) => state.cars);
+
+  //Setting the dispatch func
   const dispatch = useDispatch();
-
+  //Handle the name input change
   const handleNameChange = (event) => {
-    console.log(nameInput);
-
+    console.log(name);
     dispatch(changeName(event.target.value));
   };
+
+  //Handle the cost input change
   const handleCostChange = (event) => {
-    console.log(costInput);
-    dispatch(changeCost(+event.target.value));
+    console.log(cost);
+    dispatch(changeCost(+event.target.value || 0));
   };
+
+  //Handle the submission of the form
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addCar({ name, cost }));
+    console.log(carsList);
+  };
+
   return (
     <div className="car-form panel">
       <h4 className="subtitle is-3">Burron</h4>
-      <form className="" action="">
+      <form onSubmit={handleFormSubmit} className="" action="">
         <div className="field-group">
           <div className="field">
             <label htmlFor="nameInput" className="label">
               Name
             </label>
             <input
-              value={nameInput}
+              value={name}
               onChange={handleNameChange}
               id="nameInput"
               className="input is-expanded"
@@ -37,7 +49,7 @@ function CarForm() {
               Cost
             </label>
             <input
-              value={costInput}
+              value={cost || ""}
               onChange={handleCostChange}
               id="costInput"
               className="input is-expanded"
@@ -45,7 +57,9 @@ function CarForm() {
             />
           </div>
         </div>
-        <button>Submit</button>
+        <div className="field">
+          <button className="button is-link">Submit</button>
+        </div>
       </form>
     </div>
   );
