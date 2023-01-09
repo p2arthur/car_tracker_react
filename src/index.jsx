8f10d1050f771e505e856af4245853d5c2528f03
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-01-09 18:50:27 -0300

    add basic styling to the app

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import "./assets/styles.css";
 
 import App from "./App";
 
