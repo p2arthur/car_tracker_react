9dfbb231a361ad5393012d5b10149d483993dea9
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-01-09 19:07:44 -0300

    add highlight to possibly duplicated cars

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -3,11 +3,15 @@ import { removeCar } from "../store";
 
 function CarList() {
   //Destructuring the carsList and searchTerm from the cars state
-  const cars = useSelector(({ cars: { carsList, searchTerm } }) =>
-    //Filtering the carsList to return only the car.name that contains the search term - Only those will be displayed on the list
-    carsList.filter((car) =>
-      car.name.toLowerCase().includes(searchTerm.toLowerCase())
-    )
+  const { cars, name } = useSelector(
+    ({ cars: { carsList, searchTerm }, form }) => {
+      //Filtering the carsList to return only the car.name that contains the search term - Only those will be displayed on the list
+      const filteredCars = carsList.filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+
+      return { cars: filteredCars, name: form.name };
+    }
   );
   const dispatch = useDispatch();
 
@@ -18,19 +22,25 @@ function CarList() {
   };
 
   //Rendered cars map to display the filtered cars from the carsList
-  const renderedCars = cars.map((car) => (
-    <div key={car.id} className="panel">
-      <p>
-        {car.name} - ${car.cost}
-      </p>
-      <button
-        onClick={() => handleDeleteCar(car.id)}
-        className="button is-danger"
-      >
-        Delete
-      </button>
-    </div>
-  ));
+  const renderedCars = cars.map((car) => {
+    //Logic to decide if the car name should be bold
+
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
+    return (
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
+        <p className="">
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          onClick={() => handleDeleteCar(car.id)}
+          className="button is-danger"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
 
   return (
     <div className="car-list">
--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 
 function CarValue() {
-  //Filtering the cars that have the cars that includes the search term
+  //Filtering the cars that have the cars that includes the search term - Derived State
   const totalCost = useSelector(({ cars: { carsList, searchTerm } }) =>
     carsList
       .filter((car) =>
